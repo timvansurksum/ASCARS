facb79397511949cf0202d0f483187a0b702de73
Tim <targa2002260@gmail.com>
2023-02-17 22:53:05 +0100

    finished calibration code still need to impolement calibration into experiment data processing

--- Data_Processor.py
+++ Data_Processor.py
@@ -4,6 +4,28 @@ import pandas as pd
 class Data_Processor:
 
     @classmethod
+    def graph_Kalibration(self, kalibration_data: pd.DataFrame):
+        frequencies = list(kalibration_data.drop_duplicates(subset=['frequency']).to_dict()['frequency'].values())
+        frequency_count = len(frequencies)
+        fig, axs = plt.subplots(frequency_count, 1)
+        plt.title('calibration graphs')
+        for frequency_id in range(frequency_count):
+            frequency = frequencies[frequency_id]
+            kalibration_data_with_current_frequency = kalibration_data[kalibration_data["frequency"] == frequency] 
+            kalibration_data_dictionairy = kalibration_data_with_current_frequency.to_dict()
+            
+            microphone_intensity_values = list(kalibration_data_dictionairy['microphone_intensity'].values())
+            DB_level_values = list(kalibration_data_dictionairy['DB_level'].values())
+            
+            axs[frequency_id].plot(microphone_intensity_values, DB_level_values)
+            axs[frequency_id].set_title(f'kalibration graph of {str(frequency)}hz frequency')
+            axs[frequency_id].set_xlabel('intensity_values')
+            axs[frequency_id].set_ylabel('DB_values')
+        plt.show()
+        print('done showing calibration graphs')
+
+    
+    @classmethod
     def process_Calibration_Data(self, calibration_data, frequency):
             recording = list(map(abs, calibration_data['recording']))
             smooth_recording = self.smooth_Sound(recording, 441)
