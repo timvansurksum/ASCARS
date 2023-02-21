25c3a8e37c56d8c7289619c8bb49cdd6e579e6c5
Pieter Toorenenburg <pietertoor@gmail.com>
2023-02-21 14:52:51 +0100

    tekst en layout aangepast. bij calibration moet stop nog als variabele goed worden ingevuld. Staat nu als mogelijkheid, maar kan niet gebruikt worden.

--- Program.py
+++ Program.py
@@ -6,56 +6,66 @@ class Program:
     
     @classmethod
     def startup(self, mode):
-        print('startup...')
+        
+        print('Startup...')
+        
         if mode == 'text':
             self.start_Text_Based()
 
 
     def start_Text_Based():
-        print('running ASCARS...')
-
+        
+        print('Running ASCARS...')
         running = True
+
         while running:
-            what_to_run = input("what do you want to run?.. 'calibration', 'experiment' or 'end' to end the program?: ")
+            what_to_run = input("What do you want to run? For calibration type 'calibration', for experiment type 'experiment' or type 'end' to end the program: ")
+            
             while not (what_to_run in ['calibration', 'experiment', 'end']):
-                what_to_run = input("please enter a valid option your options are: 'calibration', 'experiment' 'end': ")
+                what_to_run = input("Please enter a valid option. The options are: 'calibration', 'experiment' 'end': ")
+            
             if what_to_run == 'calibration':
-                run_or_view_calibration = input("do you want to run an calibration, view data or end the program?\nenter 'run', 'view', or 'stop: ")
+                run_or_view_calibration = input("Do you want to run an calibration, view data or end the program?\nEnter 'run', 'view', or 'stop: ")
+                
                 while not (run_or_view_calibration in ['run', 'view']):
-                    run_or_view_calibration = input("invalid input enter either 'run' or 'view': ")
+                    run_or_view_calibration = input("Invalid input, please enter either 'run' or 'view': ")
                 
                 if run_or_view_calibration == "run":
                     Reverberation_Test.run_Calibration()
+                
                 if run_or_view_calibration == "view":
                     Reverberation_Test.show_Calibration()
-            elif what_to_run == 'experiment':
+            
+            elif what_to_run == 'experiment': 
+                run_or_view_experiment = input("Do you want to run an experiment, view data or end the program?\nEnter 'run', 'view', or 'stop: ")
                 
-                run_or_view_experiment = input("do you want to run an experiment, view data or end the program?\nenter 'run', 'view', or 'stop: ")
                 while not (run_or_view_experiment in ['run', 'view']):
-                    run_or_view_experiment = input("invalid input enter either 'run' or 'view': ")
+                    run_or_view_experiment = input("Invalid input, please enter either 'run' or 'view': ")
                 
                 if run_or_view_experiment ==  'view':
-                        print('what position do you want to run an experiment on?')
-                        x_position = str(input('enter your x position: '))
-                        y_position = str(input('enter your y position: '))
+                        print('What position do you want to run an experiment on?')
+                        x_position = str(input('Enter your x position: '))
+                        y_position = str(input('Enter your y position: '))
                         Plot_Data.graph_Experiment_Data(x_position, y_position)
+                
                 if run_or_view_experiment ==  'run':
-                    
                     running_experiments = True
-                    while running_experiments:
                     
-                        print('what position do you want to run an experiment on?')
-                        x_position = str(input('enter your x position: '))
-                        y_position = str(input('enter your y position: '))
+                    while running_experiments:
+                        print('What position do you want to run an experiment on?')
+                        x_position = str(input('Enter your x position: '))
+                        y_position = str(input('Enter your y position: '))
                         Reverberation_Test.run_Experiment(x_position, y_position)
+                        keep_running = input("Do you want to run another test? 'yes' or 'no': ")
                         
-                        keep_running = input("do you want to run another test? 'yes' or 'no': ")
                         while not (keep_running in ['yes', 'no']):
-                            keep_running = input("invalid input enter either 'yes' or 'no': ")
+                            keep_running = input("Invalid input, please enter either 'yes' or 'no': ")
                         
                         if keep_running == 'yes':
                             running_experiments = True
+                        
                         elif keep_running == 'no':
                             running_experiments = False
+            
             if what_to_run ==  'end':
                 running = False
