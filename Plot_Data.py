890b1dc167071f463d9c179703a89338b60b9b3d
Tim <targa2002260@gmail.com>
2023-03-19 15:33:01 +0100

    cleaned up Plot_Data code and added documentation

--- Plot_Data.py
+++ Plot_Data.py
@@ -8,22 +8,34 @@ class Plot_Data:
     
     @classmethod
     def graph_Experiment_Data(self, x: str, y: str, settings: dict):
+        """
+        Graphs the sound intensity of a given point
+
+        x: str, x position of the recording
+        y: str, y position of the recording
+        settings: dict, global settings data
+        """
+
+
         sample_rate = settings["sampling_rate"]
         general_data = json.loads(open(settings["data_storage_path"] + "data/reverberation_data/general_data.json", 'r').read())
         experiment_general_data = general_data["x_value"][x]["y_value"][y]
         
         frequency_to_graph_id = {}
         
+        # sets a map to get every frequency to a unique graph
         frequencies = experiment_general_data["frequencies"]
         graph_id = 1
         for frequency in  frequencies:
             frequency_to_graph_id[frequency] = graph_id
             graph_id += 1
  
+        # gathers graphing data
         graph_data = pd.read_csv(f"./data/reverberation_data/recordings/{x}_{y}.csv")
         recording = list(graph_data['recording'])
         time_data = list(graph_data['time_data'])
         
+        # creates subplots to hold each graph
         number_of_graphs = 1 + len(frequencies)
         fig, axs = plt.subplots(number_of_graphs, 1, constrained_layout=True)
         fig.suptitle(f"reverberation plot for position ({x}, {y})")
@@ -31,6 +43,7 @@ class Plot_Data:
         axs[0].set_title("whole recording intesity")
         axs[0].plot(time_data, recording)
 
+        # graphs the tone for each seperate frequency
         for frequency in frequencies:
             
             graph_id = frequency_to_graph_id[frequency]
@@ -48,6 +61,7 @@ class Plot_Data:
             axs[graph_id].plot(subsection_of_time_data_for_this_frequency, subsection_of_recording_for_this_frequency)
             axs[graph_id].set_title(f'{str(frequency)}Hz')
 
+            # graphs each datapoint in the form of lines with labels
             for vertical_line_title in vertical_lines.keys():
                 vertical_line = vertical_lines[vertical_line_title]
                 
@@ -83,8 +97,17 @@ class Plot_Data:
            
     @classmethod
     def graph_Kalibration(self, kalibration_data: pd.DataFrame):
+        """
+        Graphs the calibration of each frequency
+
+        kalibration_data: pd.DataFrame, all the calibraion data
+        """
+
+        # pulls a list of frequenties in the data from the data
         frequencies = list(kalibration_data.drop_duplicates(subset=['frequency']).to_dict()['frequency'].values())
         frequency_count = len(frequencies)
+
+        # graphs the data oer frequency taking into account there only being one frequency
         if frequency_count == 1:
             frequency = frequencies[0]
             kalibration_data_with_current_frequency = kalibration_data[kalibration_data["frequency"] == frequency] 
@@ -114,14 +137,18 @@ class Plot_Data:
                 axs[frequency_id].set_title(title)
                 axs[frequency_id].set_xlabel('intensity_values')
                 axs[frequency_id].set_ylabel('DB_values')
-                
-
         plt.show()
     
         
     @classmethod
-    def show_Heatmap(self, file_location, frequencie):
-        imported_data = Data_Processor.process_General_Data_For_Heat_Map(file_location, frequencie)
+    def show_Heatmap(self, file_location: str, frequency: int):
+        """
+        Uses the heatmap information given to show a heatmap of the room
+
+        file_location: str, location of the file that stores the general data of each positions experiment
+        frequency: int, frequency you want to show the heatmap of
+        """
+        imported_data = Data_Processor.process_General_Data_For_Heat_Map(file_location, frequency)
         transformed_data = imported_data.pivot("pos_x", "pos_y", "reverberation_time")
 
         sns.heatmap(transformed_data, vmin=0, vmax=2)
